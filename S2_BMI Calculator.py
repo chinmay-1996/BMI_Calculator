016472551d09e0f5c4d3d316af4e62d558c73eaa
chinmay-1996 <92691064+chinmay-1996@users.noreply.github.com>
2023-02-09 22:06:48 +0530

    Add files via upload

---
+++ S2_BMI Calculator.py
@@ -0,0 +1,9 @@
+def calculate_bmi(weight, height):
+    bmi = weight / (height**2)
+    return bmi
+
+weight = float(input("Enter your weight (in kg): "))
+height = float(input("Enter your height (in meters): "))
+
+bmi = calculate_bmi(weight, height)
+print("Your BMI is: {:.2f}".format(bmi))
